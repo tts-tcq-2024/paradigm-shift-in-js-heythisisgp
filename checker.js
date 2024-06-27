3e5e7b68b7d8ca7b4e2ff77ed5fd19dc8da52b5d
ganeshpandi <heythisisgp@gmail.com>
2024-06-27 10:41:33 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,32 +1,26 @@
 function batteryIsOk(temperature,  soc,  chargeRate) {
-    if(temperature < 0 || temperature > 45) {
-        console.log("Temperature is out of range!");
-        return false;
-    } else if(soc < 20 || soc > 80) {
-        console.log("State of Charge is out of range!");
-        return false;
-    } else if(chargeRate > 0.8) {
-        console.log("Charge Rate is out of range!");
-        return false;
-    }
-    return true;
+  console.log(
+    temperature < 0 || temperature > 45 ? "Temperature is out of range!" :
+    soc < 20 || soc > 80 ? "State of Charge is out of range!" :
+    chargeRate > 0.8 ? "Charge Rate is out of range!" :
+    "All parameters are within range."
+);
+    let value = ((temperature < 0 || temperature > 45) || (soc < 20 || soc > 80) || (chargeRate > 0.8)) ? false: true;
+    return value;
+
 }
 
-function  ExpectTrue(expression) {
+function ExpectTrueOrFalse(expression) {
     if(!expression) {
         console.log("Expected true, but got false");
         
-    }
-}
-function ExpectFalse(expression) {
-    if(expression) {
-        console.log("Expected false, but got true");
-        Environment.Exit(1);
+    } else {
+        console.log("Expected false, but got true"); 
     }
 }
 function main() {
-    ExpectTrue(batteryIsOk(25, 70, 0.7));
-    ExpectFalse(batteryIsOk(50, 85, 0.0));
+    ExpectTrueOrFalse(batteryIsOk(25, 70, 0.7));
+    ExpectTrueOrFalse(batteryIsOk(50, 85, 0.0));
     console.log("All ok");
     return 0;
 }
