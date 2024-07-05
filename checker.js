bf780212f2880a6d979950c87754f6c0f988222c
ganeshpandi <heythisisgp@gmail.com>
2024-07-06 01:18:16 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,30 +1,52 @@
+function checkTemperature(temperature) {
+    return temperature < 0 || temperature > 45;
+}
+
+function checkSoc(soc) {
+    return soc < 20 || soc > 80;
+}
+
+function checkChargeRate(chargeRate) {
+    return chargeRate > 0.8;
+}
+
+function logMessage(message) {
+    console.log(message);
+}
+
 function batteryIsOk(temperature, soc, chargeRate) {
-    let temperatureIsInRange = temperature >= 0 && temperature <= 45;
-    let socIsInRange = soc >= 20 && soc <= 80;
-    let chargeRateIsInRange = chargeRate <= 0.8;
+    if (checkTemperature(temperature)) {
+        logMessage("Temperature is out of range!");
+        return false;
+    }
+    if (checkSoc(soc)) {
+        logMessage("State of Charge is out of range!");
+        return false;
+    }
+    if (checkChargeRate(chargeRate)) {
+        logMessage("Charge Rate is out of range!");
+        return false;
+    }
 
-    if (!temperatureIsInRange) {
-        console.log("Temperature is out of range!");
-    }
-    if (!socIsInRange) {
-        console.log("State of Charge is out of range!");
-    }
-    if (!chargeRateIsInRange) {
-        console.log("Charge Rate is out of range!");
-    }
+    logMessage("All parameters are within range.");
+    return true;
+}
 
-    return temperatureIsInRange && socIsInRange && chargeRateIsInRange;
+function ExpectTrueOrFalse(expression) {
+    if (!expression) {
+        logMessage("Expected true, but got false");
+    } else {
+        logMessage("Expected false, but got true");
+    }
 }
 
 function main() {
-    let batteryOk = batteryIsOk(25, 70, 0.7);
-
-    if (batteryOk) {
-        console.log("All ok");
-    } else {
-        console.log("Battery is not ok");
-    }
+    ExpectTrueOrFalse(batteryIsOk(25, 70, 0.7));
+    ExpectTrueOrFalse(batteryIsOk(50, 85, 0.0));
+    logMessage("All ok");
+    return 0;
 }
 
+// Call the main function to execute the tests
 main();
 
