e0381b409ca8f71f5702c20f16d7aa32921b6999
ganeshpandi <heythisisgp@gmail.com>
2024-08-02 00:27:34 +0530

    Update batterychecker.js

--- batterychecker.js
+++ batterychecker.js
@@ -1,8 +1,26 @@
-const { testTemperature, testSocThreshold, testChargeRate } = require('./batterystatusmain');
+let currentLanguage = 'english';
+let translations = {};
 
-function batteryIsOk(temperature, soc, chargeRate) {
-    return testTemperature(temperature) && testSocThreshold(soc) && testChargeRate(chargeRate);
-}
+const loadLanguage = async lang => {
+  try {
+    const response = await fetch('translations.json');
+    const data = await response.json();
+    currentLanguage = data[lang] ? lang : currentLanguage;
+    translations = data;
+  } catch (error) {
+    console.error('Error loading translations:', error);
+  }
+};
 
-module.exports = batteryIsOk;
+const getTranslation = key => translations[currentLanguage][key] || key;
+const isInRange = (value, min, max, key) => value >= min && value <= max || !console.log(getTranslation(key));
+const batteryIsOk = (temperature, soc, chargeRate) => 
+  isInRange(temperature, 0, 45, 'temperatureOutOfRange') &&
+  isInRange(soc, 20, 80, 'socOutOfRange') &&
+  isInRange(chargeRate, 0, 0.8, 'chargeRateOutOfRange');
+
+(async () => {
+  await loadLanguage('german');
+  console.log(getTranslation(batteryIsOk(25, 70, 0.7) ? 'allOk' : 'batteryNotOk'));
+})();
 
