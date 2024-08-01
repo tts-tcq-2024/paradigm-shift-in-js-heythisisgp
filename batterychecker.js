b7bce7433d307c28f12a8c5d12a814eb0b23f479
ganeshpandi <heythisisgp@gmail.com>
2024-08-02 00:33:27 +0530

    Update batterychecker.js

--- batterychecker.js
+++ batterychecker.js
@@ -1,26 +1,29 @@
 let currentLanguage = 'english';
 let translations = {};
 
-const loadLanguage = async lang => {
-  try {
-    const response = await fetch('translations.json');
-    const data = await response.json();
-    currentLanguage = data[lang] ? lang : currentLanguage;
+fetch('translations.json')
+  .then(response => response.json())
+  .then(data => {
+    currentLanguage = 'german';
     translations = data;
-  } catch (error) {
-    console.error('Error loading translations:', error);
-  }
-};
+  })
+  .catch(error => console.error('Error loading translations:', error));
 
-const getTranslation = key => translations[currentLanguage][key] || key;
-const isInRange = (value, min, max, key) => value >= min && value <= max || !console.log(getTranslation(key));
-const batteryIsOk = (temperature, soc, chargeRate) => 
-  isInRange(temperature, 0, 45, 'temperatureOutOfRange') &&
-  isInRange(soc, 20, 80, 'socOutOfRange') &&
-  isInRange(chargeRate, 0, 0.8, 'chargeRateOutOfRange');
+function getTranslation(key) {
+  return translations[currentLanguage][key] || key;
+}
 
-(async () => {
-  await loadLanguage('german');
-  console.log(getTranslation(batteryIsOk(25, 70, 0.7) ? 'allOk' : 'batteryNotOk'));
-})();
+function isInRange(value, min, max, key) {
+  return value >= min && value <= max || console.log(getTranslation(key));
+}
+
+function batteryIsOk(temperature, soc, chargeRate) {
+  return isInRange(temperature, 0, 45, 'temperatureOutOfRange') 
+    && isInRange(soc, 20, 80, 'socOutOfRange') 
+    && isInRange(chargeRate, 0, 0.8, 'chargeRateOutOfRange');
+}
+
+batteryIsOk(25, 70, 0.7) 
+  ? console.log(getTranslation('allOk')) 
+  : console.log(getTranslation('batteryNotOk'));
 
