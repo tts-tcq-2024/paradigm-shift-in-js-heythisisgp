55a2c599cd4e92f26a8157d34896a0e6170e14e7
ganeshpandi <heythisisgp@gmail.com>
2024-08-10 01:58:15 +0530

    Update batteryChecker.js

--- batteryChecker.js
+++ batteryChecker.js
@@ -1,22 +1,5 @@
 // Language translations
-const translations = {
-  eng: {
-    temperatureOutOfRange: "Temperature is out of range!",
-    socOutOfRange: "State of Charge is out of range!",
-    chargeRateOutOfRange: "Charge Rate is out of range!",
-    allParametersWithinRange: "All parameters are within range.",
-    expectedTrueButGotFalse: "Expected true, but got false",
-    expectedFalseButGotTrue: "Expected false, but got true"
-  },
-  ger: {
-    temperatureOutOfRange: "Temperatur ist außerhalb des Bereichs!",
-    socOutOfRange: "Ladezustand ist außerhalb des Bereichs!",
-    chargeRateOutOfRange: "Ladestrom ist außerhalb des Bereichs!",
-    allParametersWithinRange: "Alle Parameter sind innerhalb des Bereichs.",
-    expectedTrueButGotFalse: "Erwartet wahr, aber bekam falsch",
-    expectedFalseButGotTrue: "Erwartet falsch, aber bekam wahr"
-  }
-};
+import { translations } from './translations.js';
 
 // Global language variable
 let language = 'eng'; // or 'ger' for German
