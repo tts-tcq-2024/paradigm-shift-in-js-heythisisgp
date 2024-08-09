0b6f75baeee1a36316c24e38ffcff253278a8967
ganeshpandi <heythisisgp@gmail.com>
2024-08-10 01:57:43 +0530

    Update and rename translations.json to translations.js

---
+++ translations.js
@@ -0,0 +1,18 @@
+export const translations = {
+  eng: {
+    temperatureOutOfRange: "Temperature is out of range!",
+    socOutOfRange: "State of Charge is out of range!",
+    chargeRateOutOfRange: "Charge Rate is out of range!",
+    allParametersWithinRange: "All parameters are within range.",
+    expectedTrueButGotFalse: "Expected true, but got false",
+    expectedFalseButGotTrue: "Expected false, but got true"
+  },
+  ger: {
+    temperatureOutOfRange: "Temperatur ist außerhalb des Bereichs!",
+    socOutOfRange: "Ladezustand ist außerhalb des Bereichs!",
+    chargeRateOutOfRange: "Ladestrom ist außerhalb des Bereichs!",
+    allParametersWithinRange: "Alle Parameter sind innerhalb des Bereichs.",
+    expectedTrueButGotFalse: "Erwartet wahr, aber bekam falsch",
+    expectedFalseButGotTrue: "Erwartet falsch, aber bekam wahr"
+  }
+};
